f6cf43edbbfa627a126a0bed781e31de4a11c68d
Aviv Haruzi <avivharuzi@gmail.com>
2021-07-20 23:04:42 +0300

    Update material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
